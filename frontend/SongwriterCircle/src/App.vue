d9b36de741e5bd0374f502c77478bf3eb55f5021
AlexIvimey <alexivimey@gmail.com>
2025-12-21 14:18:07 -0400

    default routing

--- frontend/SongwriterCircle/src/App.vue
+++ frontend/SongwriterCircle/src/App.vue
@@ -2,6 +2,14 @@
 </script>
 <template>
   <div>This is the app file</div>
+  <div>The current route for the app is {{ $route.fullPath }}</div>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
--- frontend/SongwriterCircle/src/router/index.ts
+++ frontend/SongwriterCircle/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Login from '../components/Login.vue'
+
+const routes = [
+  {
+    path: '/login', component: Login
+  },
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
